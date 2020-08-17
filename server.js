0b0fe8b4afd589a2df76e247130fbad50afa7164
Andrew Kessler <andrew.kessler95@gmail.com>
2020-08-17 16:49:06 -0600

    trying to fix heroku still

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Define middleware here
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+
   app.use(express.static("client/build"));
-}
+
 
 // Define API routes here
 
